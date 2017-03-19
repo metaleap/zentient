634e0e45c107674a24666141cb1ed647dd930ab0
metaleap <metaleap@mailbox.org>
2017-03-19 03:00:52 +0100

    [autocommit] «Windows is the best $89 solitaire game you can buy.»

--- zentient.go
+++ zentient.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+    "bufio"
+    "fmt"
+    "os"
+    "strings"
+
+    "github.com/metaleap/go-util-str"
+)
+
 func main () {
-    println("HELLO")
+    lo := false
+    scanner := bufio.NewScanner(os.Stdin)
+    for scanner.Scan() {
+        lo = !lo  ;  txt := scanner.Text()
+        fmt.Println( ustr.Ifs(lo, strings.ToLower(txt), strings.ToUpper(txt)) )
+    }
+    if err := scanner.Err() ; err != nil {
+        print(err)
+    }
 }
