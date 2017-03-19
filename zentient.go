e59b30224376db1af4643a47beeb51284f8bb500
metaleap <metaleap@mailbox.org>
2017-03-19 23:15:10 +0100

    [autocommit] «Why is it called a funny bone when it hurts so much?»

--- zentient.go
+++ zentient.go
@@ -8,7 +8,10 @@ import (
 )
 
 func main () {
+
+
     scanner := bufio.NewScanner(os.Stdin)
+    scanner.Buffer([]byte{}, 1024*1024*128)
     for scanner.Scan() {
         txt := scanner.Text()
         fmt.Println(strings.ToUpper(txt) + strings.ToLower(txt))
