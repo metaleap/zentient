d3cf46003dee76e5d97fbd539b11d3a9c1604a83
metaleap <metaleap@mailbox.org>
2017-04-20 15:46:36 +0200

    [autocommit] «Hash table has woodworm»

--- go/queriers.go
+++ go/queriers.go
@@ -1,5 +1,12 @@
 package zgo
 import (
+	"path/filepath"
+	"strings"
+
+	"github.com/metaleap/go-devgo"
+	"github.com/metaleap/go-util-misc"
+	"github.com/metaleap/go-util-slice"
+	"github.com/metaleap/go-util-str"
 	"github.com/metaleap/zentient/z"
 )
 
@@ -15,8 +22,19 @@ func (_ *zgo) QueryTool (req *z.ReqIntel) (resp *z.RespTxt) {
 	resp = &z.RespTxt{ Id: req.Id }
 	switch req.Id {
 		case "godoc":
-			resp.Warnings = []string{ "use 'godoc cmd/strings' for documentation on the strings command" }
-			resp.Result = "func ToLower(s string) string\n    ToLower returns a copy of the string s with all Unicode letters mapped\n    to their lower case.\n\n\n\nBUGS\n\n   The rule Title uses for word boundaries does not handle Unicode\n   punctuation properly.\n\n"
+			var cmd = ustr.Split(req.Sym2, " ")
+			if devgo.Has_godoc { cmd = append(ustr.N("godoc"), cmd...) } else { cmd = append(ustr.N("go", "doc"), cmd...) }
+			cmdout,cmderr,_ := ugo.CmdExecStdin ("", filepath.Dir(req.Ffp), cmd[0], cmd[1:]...)
+			resp.Warnings = uslice.StrMap(ustr.Split(cmderr, "\n"), ustr.Trim)
+			//use 'godoc cmd/strings' for documentation on the strings command \n\n
+			if ustr.Pref(cmdout, "use 'godoc cmd/") { cmdout = cmdout[ustr.Idx(cmdout, "\n"):] }
+			lns := ustr.Split(cmdout, "\n")
+			for i,ln := range lns {
+				if len(ln)>0 && !ustr.Pref(ln, " ") { lns[i] = "</p><h2>" + ustr.Trim(ln) + "</h2><p>" } else { lns[i] = ustr.Trim(ln) }
+			}
+			cmdout = strings.Replace(ustr.Trim(ustr.Join(lns, "\n")), "\n\n", "</p><p>", -1)
+			if !ustr.Pref(cmdout, "<p>") { cmdout = "<p>" + cmdout }  ;  if !ustr.Suff(cmdout, "</p>") { cmdout = cmdout + "</p>" }
+			resp.Result = cmdout
 		default:
 			resp.Warnings = []string{ "Unknown querier: " + req.Id }
 	}
