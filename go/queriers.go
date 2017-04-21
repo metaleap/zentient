8ceef5421656599aa69c3c6635e92f04d1b588fb
metaleap <metaleap@mailbox.org>
2017-04-21 10:49:07 +0200

    [autocommit] «'You can't make a program without broken egos.'»

--- go/queriers.go
+++ go/queriers.go
@@ -1,7 +1,6 @@
 package zgo
 import (
 	"path/filepath"
-	"strings"
 	"time"
 
 	"github.com/metaleap/go-devgo"
@@ -13,8 +12,8 @@ import (
 )
 
 var queryTools = []*z.RespPick {
-		&z.RespPick{ Label: "GoDoc", Detail: "[package] [member name] – shows the specified item's documentation topic.", Desc: "godoc" },
-		&z.RespPick{ Label: "go run", Detail: "any expression – attempts to evaluate the specified expression given the current source context", Desc: "gorun" },
+		&z.RespPick{ Label: "go doc", Detail: "[package] [member name] – shows the specified item's summary description.", Desc: "go doc" },
+		&z.RespPick{ Label: "go run", Detail: "any expression – attempts to evaluate the specified expression given the current source context", Desc: "go run" },
 	}
 func (me *zgo) QueryTools () []*z.RespPick {
 	return queryTools
@@ -24,7 +23,7 @@ func (me *zgo) QueryTools () []*z.RespPick {
 func (_ *zgo) QueryTool (req *z.ReqIntel) (resp *z.RespTxt) {
 	resp = &z.RespTxt{ Id: req.Id }
 	switch req.Id {
-	case "gorun":
+	case "go run":
 		/*
 [00:30 rox ~/.../metaleap/tmpprog]$ go run *.go
 go run: cannot run non-main package
@@ -47,9 +46,10 @@ runtime.main: undefined: main.main
 		*/
 		req.EnsureSrc()
 		pname := "tmp_repl_" + ugo.SPr(time.Now().UnixNano())  ;  pdir := filepath.Join(z.Ctx.CacheDir, pname)  ;  ufs.EnsureDirExists(pdir)
-		pfp := filepath.Join(pdir, "main.go")  ;  ufs.WriteTextFile(pfp, req.Src)
+		// srcdir := srcDir  ;  if len(req.Ffp)>0 { srcdir = filepath.Dir(req.Ffp) }
+
 		resp.Result = req.Src
-	case "godoc":
+	case "go doc":
 		req.Sym2 = ustr.Trim(req.Sym2)
 		if i1,i2 := ustr.Idx(req.Sym2, ".") , ustr.Idx(req.Sym2, " ")  ;  i1>0 && (i2<0 || i2>i1) { req.Sym2 = req.Sym2[:i1] + " " + req.Sym2[i1+1:] }
 		var cmd = ustr.Split(req.Sym2, " ")  ;  if devgo.PkgsByImP!=nil && ustr.IsLower(cmd[0][:1]) && devgo.PkgsByImP[cmd[0]]==nil {
@@ -59,25 +59,12 @@ runtime.main: undefined: main.main
 				if pkg.Name==cmd[0] { cmd[0] = pkg.ImportPath  ;  break }
 			} }
 		}
-		if devgo.Has_godoc { cmd = append(ustr.N("godoc", "-ex"), cmd...) } else { cmd = append(ustr.N("go", "doc"), cmd...) }
-		cmdout,cmderr,_ := ugo.CmdExecStdin ("", filepath.Dir(req.Ffp), cmd[0], cmd[1:]...)
+		cmdout,cmderr,_ := ugo.CmdExecStdin ("", filepath.Dir(req.Ffp), "go", append([]string{ "doc" }, cmd...)...)
 		resp.Warnings = uslice.StrMap(ustr.Split(cmderr, "\n"), ustr.Trim)
-		if ustr.Pref(cmdout, "use 'godoc cmd/") { cmdout = cmdout[ustr.Idx(cmdout, "\n"):] }
-		prep,p,lns := "" , 0 , ustr.Split(cmdout, "\n")  ;  for i,ln := range lns {
-			if len(ln)>0 && len(ustr.LettersOnly(ln))>0 && !ustr.Pref(ln, " ") {
-				if ln = ustr.Trim(ln)  ;  ustr.IsUpper(ln) { lns[i] = "</p><h1>" + ln + "</h1><p>" } else {
-					lns[i] = "</p><h2 id='" + ugo.SPr(p) + "'>" + ln + "</h2><p>"
-					prep += "<li><a href='#" + ugo.SPr(p) + "'>" + ln + "</a></li>"  ;  p++
-				}
-			} else { lns[i] = ustr.Trim(ln) }
-		}
-		if cmdout = strings.Replace(ustr.Trim(ustr.Join(lns, "\n")), "\n\n", "</p><p>", -1)  ;  len(cmdout)>0 {
-			if !ustr.Pref(cmdout, "<p>") { cmdout = "<p>" + cmdout }  ;  if !ustr.Suff(cmdout, "</p>") { cmdout = cmdout + "</p>" }
-		}
-		if ustr.Has(prep, "</li><li>") {  cmdout = "<ul>" + prep + "</ul>" + cmdout  }
 		resp.Result = cmdout
 	default:
 		resp.Warnings = []string{ "Unknown querier: " + req.Id }
 	}
+	if len(resp.Warnings)>1 { resp.Warnings = uslice.StrWithout(resp.Warnings, true, "exit status 1") }
 	return
 }
