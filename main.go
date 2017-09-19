5a5718529b16d07b510c2b291ac6461f2b120586
metaleap <metaleap@mailbox.org>
2017-09-19 20:09:20 +0200

    [autocommit] «If you wish to succeed, consult three old people.»

--- main.go
+++ main.go
@@ -1,25 +1,27 @@
 package main
+
 import (
 	"bufio"
 	"runtime"
 
 	"github.com/metaleap/go-util-misc"
 
-	"github.com/metaleap/zentient/z"
 	"github.com/metaleap/zentient/go"
 	"github.com/metaleap/zentient/hs"
+	"github.com/metaleap/zentient/z"
 )
 
-
-func main () {
+func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU() * 2)
-	err := z.Init(map[string]func()z.Zengine { "go": zgo.Init, "hs": zhs.Init })
-	if err!=nil { panic(err) }
+	err := z.Init(map[string]func() z.Zengine{"go": zgo.Init, "hs": zhs.Init})
+	if err != nil {
+		panic(err)
+	}
 
 	var stdin *bufio.Scanner
-	stdin,z.RawOut,z.Out = ugo.SetupJsonProtoPipes(1024*1024*4, false, true)
+	stdin, z.RawOut, z.Out = ugo.SetupJsonProtoPipes(1024*1024*4, false, true)
 	for stdin.Scan() {
-		if err = z.HandleRequest(stdin.Text()) ; err == nil {
+		if err = z.HandleRequest(stdin.Text()); err == nil {
 			err = z.RawOut.Flush()
 		}
 		if err != nil {
