b2bca24657c81f1e923c297373787987c66b0838
metaleap <metaleap@users.noreply.github.com>
2018-06-17 14:00:48 +0200

    [autopush] «Life is like a diaper -- short and loaded.»

---
+++ lang/golang/°gent.go
@@ -0,0 +1,3 @@
+package zgo
+
+// DO NOT EDIT: code generated with zentient-dbg-vsc-go-demo-go-gent using github.com/metaleap/go-gent
---
+++ °gent.go
@@ -0,0 +1,10 @@
+package z
+
+// DO NOT EDIT: code generated with zentient-dbg-vsc-go-demo-go-gent using github.com/metaleap/go-gent
+
+func (this CaddyStatus) Valid() (ret bool)  { return }
+func (this IpcIDs) Valid() (ret bool)       { return }
+func (this DiagSeverity) Valid() (ret bool) { return }
+func (this Symbol) Valid() (ret bool)       { return }
+func (this Completion) Valid() (ret bool)   { return }
+func (this ToolCats) Valid() (ret bool)     { return }
