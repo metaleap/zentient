47f2c622844fbe3f2e30d8c588c055c37fc72630
metaleap <metaleap@users.noreply.github.com>
2019-05-06 21:51:22 +0200

    atmosess: improve sessDir logic for lang-server / editor usage

---
+++ lang/atmo/zat-src-intel.go
@@ -0,0 +1,15 @@
+package zat
+
+import (
+	"github.com/metaleap/zentient"
+)
+
+var srcIntel atmoSrcIntel
+
+type atmoSrcIntel struct {
+	z.SrcIntelBase
+}
+
+func init() {
+	srcIntel.Impl, z.Lang.SrcIntel = &srcIntel, &srcIntel
+}
