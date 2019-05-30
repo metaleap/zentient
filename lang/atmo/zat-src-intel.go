3e465da2ff78030adc2bb572625f1800e2b1057e
metaleap <metaleap@users.noreply.github.com>
2019-05-30 17:15:07 +0200

    beginning: editor integration helpers

--- lang/atmo/zat-src-intel.go
+++ lang/atmo/zat-src-intel.go
@@ -1,8 +1,11 @@
 package zat
 
 import (
+	"fmt"
 	"path/filepath"
 
+	// "github.com/metaleap/atmo"
+	"github.com/metaleap/atmo/lang"
 	"github.com/metaleap/zentient"
 )
 
@@ -16,11 +19,27 @@ func init() {
 	srcIntel.Impl, z.Lang.SrcIntel = &srcIntel, &srcIntel
 }
 
-func (me *atmoSrcIntel) DefSym(srcLens *z.SrcLens) (locs z.SrcLocs) {
+func (me *atmoSrcIntel) References(srcLens *z.SrcLens, includeDeclaration bool) (locs z.SrcLocs) {
 	if kit := Ctx.KitByDirPath(filepath.Dir(srcLens.FilePath), true); kit != nil {
-		if _, nodes := kit.AstNodeAt(srcLens.FilePath, srcLens.ByteOffsetForPos(srcLens.Pos)); len(nodes) > 0 {
+		if tlc, nodes := kit.AstNodeAt(srcLens.FilePath, srcLens.ByteOffsetForPos(srcLens.Pos)); len(nodes) > 0 {
+			if tok := nodes[len(nodes)-1].Toks().First(nil); tok != nil {
+				locs = append(locs, &z.SrcLoc{FilePath: tlc.SrcFile.SrcFilePath,
+					Pos: &z.SrcPos{Off: tok.Meta.Offset + 1, Ln: tok.Meta.Line, Col: tok.Meta.Column}})
+			}
 
+			if ident, _ := nodes[len(nodes)-1].(*atmolang.AstIdent); ident != nil && ident.IsName(true) {
+				for _, kit := range Ctx.Kits.All {
+					for _, def := range kit.Defs(ident.Val) {
+						if tok := def.Name.OrigToks().First(nil); tok != nil {
+							locs = append(locs, &z.SrcLoc{FilePath: def.OrigTopLevelChunk.SrcFile.SrcFilePath,
+								Pos: &z.SrcPos{Off: tok.Meta.Offset + 1, Ln: tok.Meta.Line, Col: tok.Meta.Column}})
+						}
+					}
+				}
+			}
 		}
+	} else {
+		z.SendNotificationMessageToClient(z.DIAG_SEV_INFO, fmt.Sprintf("%v", len(Ctx.Kits.All)))
 	}
 	return
 }
