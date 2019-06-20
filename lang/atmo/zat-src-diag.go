4402cb821450cc34ace769234d74a67cbadd1cd7
metaleap <metaleap@users.noreply.github.com>
2019-06-20 12:37:30 +0200

    live-mode: ensure diag position ranges where present (followup)

--- lang/atmo/zat-src-diag.go
+++ lang/atmo/zat-src-diag.go
@@ -23,15 +23,16 @@ func (me *atDiag) onSomeKitsReprocessed() {
 	for _, kit := range Ctx.Kits.All {
 		errs2srcs := make(map[error][]byte, 2)
 		for _, err := range kit.Errors(errs2srcs) {
-			errdiag := &z.DiagItem{Msg: err.Error(), Misc: make([]interface{}, 3)}
+			errdiag := &z.DiagItem{Msg: err.Error()}
 			if e, _ := err.(*atmo.Error); e != nil {
-				errdiag.Misc[0] = e
 				errdiag.Msg, errdiag.Cat = e.Msg(), e.Cat().String()
 				if e.Cat() == atmo.ErrCatUnreachable {
 					errdiag.Tags = []int{1}
 				}
 				if pos, src := e.Pos(), string(errs2srcs[err]); pos != nil {
-					errdiag.Misc[1], errdiag.Misc[2] = pos, src
+					if errlen := e.Len(); errlen > 1 && len(src) > 0 {
+						errdiag.Misc = []interface{}{pos, src, errlen}
+					}
 					errdiag.Loc.FilePath, errdiag.Loc.Pos = pos.Filename, &z.SrcPos{}
 					if errdiag.Loc.Pos.Ln, errdiag.Loc.Pos.Col = pos.Line, pos.Column; len(src) > 0 {
 						errdiag.Loc.Pos.SetRune1OffFromByte0Off(pos.Offset, src)
@@ -60,18 +61,12 @@ func (me *atDiag) OnUpdateBuildDiags(workspaceFiles z.WorkspaceFiles, writtenFil
 
 func (me *atDiag) RunBuildJobs(jobs z.DiagBuildJobs, workspaceFiles z.WorkspaceFiles) z.DiagItems {
 	for _, errdiag := range me.errDiags {
-		if errdiag.Loc.Range == nil && errdiag.Loc.Pos != nil {
-			if e, _ := errdiag.Misc[0].(*atmo.Error); e != nil {
-				if pos, _ := errdiag.Misc[1].(*scanner.Position); pos != nil {
-					if src, ok := errdiag.Misc[2].(string); ok {
-						isfileopen := workspaceFiles.IsOpen(pos.Filename)
-						if l := e.Len(); l > 1 && len(src) > 0 && isfileopen {
-							errdiag.Loc.Range = &z.SrcRange{}
-							errdiag.Loc.Range.Start.SetRune1OffFromByte0Off(pos.Offset, src)
-							errdiag.Loc.Range.End.SetRune1OffFromByte0Off(pos.Offset+l, src)
-						}
-					}
-				}
+		if errdiag.Loc.Range == nil && errdiag.Loc.Pos != nil && errdiag.Misc != nil {
+			pos, src, errlen := errdiag.Misc[0].(*scanner.Position), errdiag.Misc[1].(string), errdiag.Misc[2].(int)
+			if workspaceFiles.IsOpen(pos.Filename) {
+				errdiag.Loc.Range = &z.SrcRange{}
+				errdiag.Loc.Range.Start.SetRune1OffFromByte0Off(pos.Offset, src)
+				errdiag.Loc.Range.End.SetRune1OffFromByte0Off(pos.Offset+errlen, src)
 			}
 		}
 	}
