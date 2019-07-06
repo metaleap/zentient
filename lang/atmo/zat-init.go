029035acd95ee6987bffc48fd1451a68598bcc55
metaleap <metaleap@users.noreply.github.com>
2019-07-06 21:16:58 +0200

    bgmessages in zat

--- lang/atmo/zat-init.go
+++ lang/atmo/zat-init.go
@@ -1,6 +1,7 @@
 package zat
 
 import (
+	"github.com/go-leap/str"
 	"github.com/metaleap/atmo/session"
 	"github.com/metaleap/zentient"
 )
@@ -15,11 +16,20 @@ func OnPreInit() (err error) {
 		Ctx, z.Lang.Enabled, z.Lang.ID, z.Lang.Title, z.Lang.Live =
 			&ctx, true, "atmo", "atmo", liveMode
 		workspace.onPreInit()
-		Ctx.Kits.OnRefreshed = diag.updateFromErrs
+		Ctx.On.SomeKitsRefreshed = diag.updateFromErrs
+		Ctx.On.NewBackgroundMessages = onNewBackgroundMessages
 		diag.updateFromErrs(true)
+		onNewBackgroundMessages()
 	}
 	return
 }
 
 func OnPostInit() {
+}
+
+func onNewBackgroundMessages() {
+	msgs := Ctx.BackgroundMessages(true)
+	for i := range msgs {
+		println(msgs[i].Time.Format("15:04:05") + "\t" + ustr.Join(msgs[i].Lines, "\n\t"))
+	}
 }
