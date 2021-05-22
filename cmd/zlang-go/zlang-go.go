bb4a6879873f03c13d91e6127ac675420ec101ab
metaleap <metaleap@users.noreply.github.com>
2021-05-22 14:44:46 +0200

    [autopush] «Lensmen eat Jedi for breakfast.»

--- cmd/zlang-go/zlang-go.go
+++ cmd/zlang-go/zlang-go.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"os"
+
 	"github.com/metaleap/zentient"
 	"github.com/metaleap/zentient/lang/golang"
 )
 
 func main() {
+	os.Setenv("GO111MODULE", "off")
 	z.InitAndServe(zgo.OnPreInit, zgo.OnPostInit)
 }
